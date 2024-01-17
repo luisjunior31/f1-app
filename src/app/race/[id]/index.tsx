558280cc2754458d92d6043335148eba911ef406
danieljunior32 <juniormperalta31@gmail.com>
2024-01-17 01:25:50 -0400

    detalles circuito

--- src/app/race/[id]/index.tsx
+++ src/app/race/[id]/index.tsx
@@ -1,16 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import raceResponse from '../../../../assets/data/race.json'
+
+
+const race = raceResponse.data.races.response[0]
 
 const RaceDetail = () => {
 
   const { id } = useLocalSearchParams()
 
   return (
-    <View>
-      <Text>RaceDetail: {id}</Text>
+    <View style={styles.page}>
+      <Text style={{
+        fontSize: 22,
+        fontFamily: 'F1-Regular'
+      }}>
+       <Text style={{ fontFamily: 'F1-Bold' }}>
+        {
+          race.competition.location.country 
+        }
+        {' '}
+       </Text>
+
+        {race.season}
+        </Text>
+
+        <Text>
+          {race.circuit.name}
+        </Text>
+
+        <Image
+         source={{ uri: race.circuit.image }}
+         style={styles.img}
+         resizeMode='contain'
+         />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 10
+  },
+  img: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+    marginVertical: 15
+  }
+})
 
 export default RaceDetail
